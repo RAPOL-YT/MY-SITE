5fd17ac3db5402876fc935f7f128f3243e60c270
RAPOL.YT <obaba9258@gmail.com>
2026-08-24 18:35:26 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,22 @@
+
+// =====================================================
+// SUPABASE CONFIGURATION
+// =====================================================
+
+// ضع هنا Project URL الخاص بمشروع Supabase
+export const SUPABASE_URL =
+    "ضع_رابط_مشروع_Supabase_هنا";
+
+
+// ضع هنا Publishable Key الخاص بمشروع Supabase
+export const SUPABASE_PUBLISHABLE_KEY =
+    "ضع_Publishable_Key_هنا";
+
+
+// =====================================================
+// ABOUT PAGE
+// =====================================================
+
+// رابط صفحة "المزيد عني"
+export const ABOUT_URL = "about.html";
+
